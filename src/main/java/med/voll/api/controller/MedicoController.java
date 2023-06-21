6e1369a4598eca5b87a84548f1c9cfe8e904ca31
gbmira <rm95694@fiap.com.br>
2023-06-21 00:09:22 -0300

    adicionado configuração do banco e classes

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,20 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
