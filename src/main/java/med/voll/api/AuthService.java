091798c60d03256e339d8186d4140bd0281a2802
gbmira <rm95694@fiap.com.br>
2023-06-25 17:04:02 -0300

    usuario respository, service, entidade e migration

---
+++ src/main/java/med/voll/api/AuthService.java
@@ -0,0 +1,18 @@
+package med.voll.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
