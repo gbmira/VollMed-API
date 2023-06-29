ca11164bfed9f8ee4f44323e3a1b0a205b9eed61
gbmira <rm95694@fiap.com.br>
2023-06-29 00:21:29 -0300

    atualizações usuario e token

---
+++ src/main/java/med/voll/api/controller/AuthController.java
@@ -0,0 +1,34 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.usuario.DadosAuth;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager am;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity login(@RequestBody @Valid DadosAuth dados){
+        var authToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var auth = am.authenticate(authToken);
+        var token = tokenService.gerarToken((Usuario) auth.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(token));
+    }
+}
